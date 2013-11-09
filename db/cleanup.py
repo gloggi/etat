270ae19d5d476974b6befc13b4516d09e5318f6e
Stefan Reinhard <sr@feinheit.ch>
2013-11-09 03:08:41 +0100

    cleanup phone numbers

--- db/cleanup.py
+++ db/cleanup.py
@@ -10,8 +10,10 @@ execfile(activate_this, dict(__file__=activate_this))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "etat.settings")
 sys.path.append('..')
 
+from django.db.models import Count
+
 from etat.departments.models import Department, DepartmentType
-from etat.members.models import Member, RoleType
+from etat.members.models import Member, RoleType, Reachability
 
 relevant_departments = ('Glockenhof', 'APV Glockenhof', 'HV Glockenhof')
 
@@ -31,8 +33,8 @@ for r in RoleType.objects.all():
         r.delete()
 
 print "Removing members without any roles"
-for m in Member.objects.all():
-    if m.roles.count() == 0:
+for m in Member.objects.annotate(role_count=Count('roles')):
+    if m.role_count == 0:
         m.delete()
 
 print "Move Abteilunsgsstäbe"
@@ -53,3 +55,14 @@ print "No members in Korpsstab"
 for r in Department.objects.get(name="Glockenhof").roles.all():
     if r.type.name == "Mitglied / TeilnehmerIn":
         r.delete()
+
+print "Make mobile phone numbers private"
+for r in Reachability.objects.filter(type='phone', value__startswith='07'):
+    if not r.kind == 'private':
+        r.kind = 'private'
+        r.save()
+
+print "Update old phone preselection"
+for r in Reachability.objects.filter(type='phone', value__startswith='01'):
+    r.value = r.value.replace('01/', '044 ').replace('01 ', '044 ')
+    r.save()
