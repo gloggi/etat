03d8e8e76a354a69e5092a180f96cf3f9cdc4d6f
Stefan Reinhard <sr@feinheit.ch>
2013-11-11 01:47:18 +0100

    redirect to member list for now

--- etat/dashboard/views.py
+++ etat/dashboard/views.py
@@ -1,6 +1,6 @@
 
-from django.shortcuts import render
-
+from django.shortcuts import redirect
 
 def home(request):
-    return render(request, 'dashboard/home.html')
+    #return render(request, 'dashboard/home.html')
+    return redirect('member_list')
